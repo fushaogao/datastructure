c7366bb586088c6f6700cafbf611c1d9fd780730
fushaogao <fushaogao@gmail.com>
2021-07-12 16:39:54 +0800

    Update QuickSort.java

--- src/main/java/com/data/sort/QuickSort.java
+++ src/main/java/com/data/sort/QuickSort.java
@@ -1,20 +1,57 @@
 package com.data.sort;
 
+import java.util.Arrays;
+
 /**
- * 快速排序
+ * 6.快速排序
+ *
+ * 算法步骤
+ * 1.从数列中挑出一个元素，称为 “基准”（pivot）;
+ * 2.重新排序数列，所有元素比基准值小的摆放在基准前面，所有元素比基准值大的摆在基准的后面（相同的数可以到任一边）。在这个分区退出之后，该基准就处于数列的中间位置。这个称为分区（partition）操作；
+ * 3.递归地（recursive）把小于基准值元素的子数列和大于基准值元素的子数列排序；
+ *
  */
-public class QuickSort {
+public class QuickSort   {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws Exception {
+        QuickSort quickSort = new QuickSort();
+        int[] arr = {3,6,2,7,3,12,7};
+        System.out.print(Arrays.toString(quickSort.sort(arr)));
     }
 
-    public static int[] quickSort(int[] arr,int low,int high){
+    public int[] sort(int[] sourceArray) throws Exception {
+        // 对 arr 进行拷贝，不改变参数内容
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
 
-        int pivot = arr[0];
-        for(int i=0;i<arr.length;i++){
+        return quickSort(arr, 0, arr.length - 1);
+    }
 
+    private int[] quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition(arr, left, right);
+            quickSort(arr, left, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, right);
         }
-        return  null;
+        return arr;
+    }
+
+    private int partition(int[] arr, int left, int right) {
+        // 设定基准值（pivot）
+        int pivot = left;
+        int index = pivot + 1;
+        for (int i = index; i <= right; i++) {
+            if (arr[i] < arr[pivot]) {
+                swap(arr, i, index);
+                index++;
+            }
+        }
+        swap(arr, pivot, index - 1);
+        return index - 1;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
