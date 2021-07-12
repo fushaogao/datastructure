e8e836cc17ec07cefe0faaab2ed3109a55bdf1db
fushaogao <fushaogao@gmail.com>
2021-07-12 19:15:02 +0800

    Create MyStack.java

---
+++ src/main/java/com/data/algorithm/stack/MyStack.java
@@ -0,0 +1,13 @@
+package com.data.algorithm.stack;
+
+
+public interface  MyStack<Item> extends Iterable<Item> {
+
+    MyStack<Item> push(Item item);
+
+    Item pop() throws Exception;
+
+    boolean isEmpty();
+
+    int size();
+}
