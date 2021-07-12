aad660efe07f48b8fb49d6a6876de3780d5542f3
fushaogao <fushaogao@gmail.com>
2021-07-12 16:36:59 +0800

    Update BubbleSort.java

--- src/main/java/com/data/sort/BubbleSort.java
+++ src/main/java/com/data/sort/BubbleSort.java
@@ -1,11 +1,41 @@
 package com.data.sort;
 
+import java.util.Arrays;
+
 /**
- * 冒泡排序
+ * 1.冒泡排序
+ * 算法步骤
+ * 1.比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+ * 2.对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。这步做完后，最后的元素会是最大的数。
+ * 3.针对所有的元素重复以上的步骤，除了最后一个。
+ * 4.持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较
  */
 public class BubbleSort {
+    public static void main(String[] args) throws Exception {
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] arr = {3,6,2,7,3,12,7};
+        System.out.print(Arrays.toString(bubbleSort.sort(arr)));
+    }
+    public int[] sort(int[] sourceArray) throws Exception {
+        // 对 arr 进行拷贝，不改变参数内容
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
 
-    public static void main(String[] args){
+        for (int i = 1; i < arr.length; i++) {
+            // 设定一个标记，若为true，则表示此次循环没有进行交换，也就是待排序列已经有序，排序已经完成。
+            boolean flag = true;
 
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                    flag = false;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+        return arr;
     }
 }
