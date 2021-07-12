1f67203279fd58c92584be183ae4e3eae1fdcc06
fushaogao <fushaogao@gmail.com>
2021-07-12 19:15:48 +0800

    Create ListStack.java

---
+++ src/main/java/com/data/algorithm/stack/ListStack.java
@@ -0,0 +1,88 @@
+package com.data.algorithm.stack;
+
+import java.util.Iterator;
+
+public class ListStack<Item> implements MyStack<Item> {
+    private Node top = null;
+    private int N = 0;
+
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+
+    @Override
+    public MyStack<Item> push(Item item) {
+
+        Node newTop = new Node();
+
+        newTop.item = item;
+        newTop.next = top;
+
+        top = newTop;
+
+        N++;
+
+        return this;
+    }
+
+
+    @Override
+    public Item pop() throws Exception {
+
+        if (isEmpty()) {
+            throw new Exception("stack is empty");
+        }
+
+        Item item = top.item;
+
+        top = top.next;
+        N--;
+
+        return item;
+    }
+
+
+    @Override
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+
+    @Override
+    public int size() {
+        return N;
+    }
+
+
+    @Override
+    public Iterator<Item> iterator() {
+
+        return new Iterator<Item>() {
+
+            private Node cur = top;
+
+
+            @Override
+            public boolean hasNext() {
+                return cur != null;
+            }
+
+
+            @Override
+            public Item next() {
+                Item item = cur.item;
+                cur = cur.next;
+                return item;
+            }
+
+            @Override
+            public void remove() {
+
+            }
+        };
+
+    }
+}
