c787da61aeeb8c3c31c047376aa877c96d3d12dd
fushaogao <fushaogao@gmail.com>
2021-07-12 16:38:48 +0800

    Update HeapSort.java

--- src/main/java/com/data/sort/HeapSort.java
+++ src/main/java/com/data/sort/HeapSort.java
@@ -1,10 +1,65 @@
 package com.data.sort;
 
+import java.util.Arrays;
+
 /**
- * 堆排序
+ * 7.堆排序
+ * 算法步骤
+ * 1.将待排序序列构建成一个堆 H[0……n-1]，根据（升序降序需求）选择大顶堆或小顶堆；
+ * 2.把堆首（最大值）和堆尾互换；
+ * 3.把堆的尺寸缩小 1，并调用 shift_down(0)，目的是把新的数组顶端数据调整到相应位置；
+ * 4.重复步骤 2，直到堆的尺寸为 1。
+ *
  */
 public class HeapSort {
     public static void main(String[] args){
 
+    }
+
+    public int[] sort(int[] sourceArray) throws Exception {
+        // 对 arr 进行拷贝，不改变参数内容
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+
+        int len = arr.length;
+
+        buildMaxHeap(arr, len);
+
+        for (int i = len - 1; i > 0; i--) {
+            swap(arr, 0, i);
+            len--;
+            heapify(arr, 0, len);
+        }
+        return arr;
+    }
+
+    private void buildMaxHeap(int[] arr, int len) {
+        for (int i = (int) Math.floor(len / 2); i >= 0; i--) {
+            heapify(arr, i, len);
+        }
+    }
+
+    private void heapify(int[] arr, int i, int len) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest = i;
+
+        if (left < len && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < len && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            swap(arr, i, largest);
+            heapify(arr, largest, len);
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
