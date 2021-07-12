812cba5bbcea45f8002fd3c4ec7c5ad805166df9
fushaogao <fushaogao@gmail.com>
2021-07-12 16:36:18 +0800

    Update ShellSort.java

--- src/main/java/com/data/sort/ShellSort.java
+++ src/main/java/com/data/sort/ShellSort.java
@@ -1,12 +1,45 @@
 package com.data.sort;
 
+import java.util.Arrays;
+
 /**
- * 希尔排序
+ * 4.希尔排序，是插入排序的一种更高效的改进版本
+ * 基本思想是：先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序，待整个序列中的记录“基本有序”时，再对全体记录进行依次直接插入排序。
+ * 算法步骤
+ * 1.选择一个增量序列 t1，t2，……，tk，其中 ti > tj, tk = 1；
+ * 2.按增量序列个数 k，对序列进行 k 趟排序；
+ * 3.每趟排序，根据对应的增量 ti，将待排序列分割成若干长度为 m 的子序列，分别对各子表进行直接插入排序。仅增量因子为 1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
+ *
  */
 public class ShellSort {
 
     public static void main(String[] args){
 
+    }
+
+    public int[] sort(int[] sourceArray) throws Exception {
+        // 对 arr 进行拷贝，不改变参数内容
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+
+        int gap = 1;
+        while (gap < arr.length/3) {
+            gap = gap * 3 + 1;
+        }
+
+        while (gap > 0) {
+            for (int i = gap; i < arr.length; i++) {
+                int tmp = arr[i];
+                int j = i - gap;
+                while (j >= 0 && arr[j] > tmp) {
+                    arr[j + gap] = arr[j];
+                    j -= gap;
+                }
+                arr[j + gap] = tmp;
+            }
+            gap = (int) Math.floor(gap / 3);
+        }
+
+        return arr;
     }
 
 
