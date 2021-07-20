fe758af381a499e77327bcd32ac659c2fd1fc71b
fushaogao <fushaogao@gmail.com>
2021-07-20 17:41:54 +0800

    Create ListNode.java

---
+++ src/main/java/com/data/model/ListNode.java
@@ -0,0 +1,24 @@
+package com.data.model;
+
+public class ListNode {
+
+    private ListNode next;
+
+    private int value;
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
