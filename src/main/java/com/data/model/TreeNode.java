a6fa702f45c0d5fff8d17b442403a9878d329094
fushaogao <fushaogao@gmail.com>
2021-07-20 17:42:11 +0800

    Create TreeNode.java

---
+++ src/main/java/com/data/model/TreeNode.java
@@ -0,0 +1,50 @@
+package com.data.model;
+
+public class TreeNode {
+
+    private TreeNode left;
+
+    private TreeNode right;
+
+    private int value;
+
+    private boolean flag;
+
+    public TreeNode(TreeNode left, TreeNode right, int value) {
+        this.left = left;
+        this.right = right;
+        this.value = value;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+}
